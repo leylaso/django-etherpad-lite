e8f75d55ae4e53f0832715740aedd19ec0fb4680
Sofian Benaissa <sofian@koumbit.org>
2012-03-08 23:03:54 -0500

    Initial schema for etherpad lite integration with django

---
+++ models.py
@@ -0,0 +1,37 @@
+from django.db import models
+from django.contrib.auth.models import User, Group
+
+# Create your models here.
+
+class PadServer(models.Model):
+  title = models.CharField(max_length=256)
+  url = models.URLField(max_length=256)
+  apikey = models.CharField(max_length=256) 
+  notes = models.TextField('Description', blank=True)
+  def __unicode__(self):
+    return self.url
+
+class PadGroup(models.Model):
+  group = models.ForeignKey(Group, verbose_name='Group')
+  groupID = models.CharField(max_length=256)
+  server = models.ForeignKey(PadServer, verbose_name='Serveur')
+  def __unicode__(self):
+    return self.groupID
+
+class PadAuthor(models.Model):
+  user = models.ForeignKey(User, verbose_name='User')
+  authorID = models.CharField(max_length=256)
+  server = models.ForeignKey(PadServer, verbose_name='Serveur')
+  group = models.ManyToManyField(PadGroup, blank=True, null=True, verbose_name='Group')
+  sessionID = models.CharField(max_length=256, blank=True)
+  def __unicode__(self):
+    return self.authorID
+
+class Pad(models.Model):
+  name = models.CharField(max_length=256)
+  server = models.ForeignKey(PadServer, verbose_name='Serveur')
+  group = models.ForeignKey(PadGroup, verbose_name='Group')
+  def __unicode__(self):
+    return self.name
+
+
