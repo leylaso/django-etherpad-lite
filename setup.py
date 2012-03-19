61c03c966e946c5b30977edbd15a7ea5d5146ff0
Nikolai Prokoschenko <nikolai.prokoschenko@arbeitsagentur.de>
2012-03-19 17:45:46 +0100

    Wrong version suffix corrected

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='django_etherpad_lite',
-    version='0.1dev',
+    version='0.1',
     author='Sofian Benaissa',
     url='https://github.com/sfyn/django_etherpad_lite',
     description='Etherpad-lite integration for Django',
