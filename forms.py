adbbd7cbaa64c100ab200aa3d30741049e4fb0b4
Sofian Benaissa <sofian@koumbit.org>
2012-03-19 21:00:02 -0400

    Improved pad creation form

--- forms.py
+++ forms.py
@@ -1,5 +1,6 @@
 from django import forms
+from django.utils.translation import ugettext as _
 
 class PadCreate(forms.Form):
-  name = forms.CharField()
+  name = forms.CharField(label=_("Name"))
   group = forms.CharField(widget=forms.HiddenInput)
