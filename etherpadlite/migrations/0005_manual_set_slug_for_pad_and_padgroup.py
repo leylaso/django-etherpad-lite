66b2bddbebac66de12bb065a40e01109f774cf05
Max Brauer <max.brauer@inqbus.de>
2012-11-29 11:12:56 +0100

    Bugfix in models.py

--- etherpadlite/migrations/0005_manual_set_slug_for_pad_and_padgroup.py
+++ etherpadlite/migrations/0005_manual_set_slug_for_pad_and_padgroup.py
@@ -2,17 +2,19 @@
 from south.v2 import SchemaMigration
 from django.template.defaultfilters import slugify
 
+from etherpadlite.models import Pad, PadGroup
+
 
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
         # Adding slug to field 'PadGroup.slug'
-        for i in orm['etherpadlite.PadGroup'].all():
+        for i in PadGroup.objects.all():
             i.slug = slugify(i.group.name)
             i.save()
 
         # Adding slug to field 'Pad.slug'
-        for i in orm['etherpadlite.Pad'].all():
+        for i in Pad.objects.all():
             i.slug = slugify(i.name)
             i.save()
 
